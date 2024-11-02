0fc2d18d35c5b4c258c795c662a3aa954d050bf4
Uddeshya Singh <singhuddeshyaofficial@gmail.com>
2024-11-02 21:56:01 +0530

    Complete disk block storage

--- mini-lsm-starter/src/block.rs
+++ mini-lsm-starter/src/block.rs
@@ -5,7 +5,7 @@ mod builder;
 mod iterator;
 
 pub use builder::BlockBuilder;
-use bytes::Bytes;
+use bytes::{Buf, BufMut, Bytes, BytesMut};
 pub use iterator::BlockIterator;
 
 /// A block is the smallest unit of read and caching in LSM tree. It is a collection of sorted key-value pairs.
@@ -18,11 +18,55 @@ impl Block {
     /// Encode the internal data to the data layout illustrated in the tutorial
     /// Note: You may want to recheck if any of the expected field is missing from your output
     pub fn encode(&self) -> Bytes {
-        unimplemented!()
+        let elements = self.offsets.len() as u16;
+        let mut buf = BytesMut::with_capacity(self.data.len() + self.offsets.len() + 2);
+        for data in self.data.clone() {
+            buf.put_u8(data);
+        }
+        for offset in &self.offsets {
+            buf.put_u16(*offset);
+        }
+        buf.put_u16(elements);
+        buf.freeze()
     }
 
     /// Decode from the data layout, transform the input `data` to a single `Block`
     pub fn decode(data: &[u8]) -> Self {
-        unimplemented!()
+        let data_len = data.len();
+        if data_len < 2 {
+            return Self {
+                data: vec![],
+                offsets: vec![],
+            };
+        }
+        let mut bytes = Bytes::from(data.to_vec());
+        let elements = ((bytes[data_len - 2] as u16) << 8) | bytes[data_len - 1] as u16;
+        let mut data = vec![];
+        for _ in 0..elements {
+            let kl_high = bytes.get_u8();
+            let kl_low = bytes.get_u8();
+            let kl = ((kl_high as u16) << 8) | kl_low as u16;
+
+            let key = bytes.split_to(kl as usize);
+
+            let vl_high = bytes.get_u8();
+            let vl_low = bytes.get_u8();
+            let vl = ((vl_high as u16) << 8) | vl_low as u16;
+
+            let value = bytes.split_to(vl as usize);
+
+            let mut curr_data = vec![kl_high, kl_low];
+            curr_data.extend(key.to_vec());
+            curr_data.push(vl_high);
+            curr_data.push(vl_low);
+            curr_data.extend(value.to_vec());
+            data.extend(curr_data);
+        }
+        let mut offsets = vec![];
+        for _ in 0..elements {
+            offsets.push(bytes.get_u16());
+        }
+
+        Self { data, offsets }
     }
 }
--- mini-lsm-starter/src/block/builder.rs
+++ mini-lsm-starter/src/block/builder.rs
@@ -2,6 +2,7 @@
 #![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
 
 use crate::key::{KeySlice, KeyVec};
+use nom::character::complete::u16;
 
 use super::Block;
 
@@ -17,25 +18,74 @@ pub struct BlockBuilder {
     first_key: KeyVec,
 }
 
+fn split_u16_to_u8s(value: u16) -> (u8, u8) {
+    let high_byte = (value >> 8) as u8; // Extract the higher 8 bits
+    let low_byte = (value & 0xFF) as u8; // Extract the lower 8 bits
+    (high_byte, low_byte)
+}
+
 impl BlockBuilder {
     /// Creates a new block builder.
     pub fn new(block_size: usize) -> Self {
-        unimplemented!()
+        Self {
+            offsets: vec![],
+            data: vec![],
+            block_size,
+            first_key: Default::default(),
+        }
     }
 
     /// Adds a key-value pair to the block. Returns false when the block is full.
     #[must_use]
     pub fn add(&mut self, key: KeySlice, value: &[u8]) -> bool {
-        unimplemented!()
+        // it's the first key addition
+        if self.data.is_empty() {
+            self.offsets.push(0);
+            self.first_key = key.to_key_vec();
+
+            let (high, low) = split_u16_to_u8s(key.len() as u16);
+            let mut entry = vec![high, low];
+            entry.extend_from_slice(key.raw_ref());
+
+            let (high_v, low_v) = split_u16_to_u8s(value.len() as u16);
+            entry.push(high_v);
+            entry.push(low_v);
+
+            entry.extend_from_slice(value);
+            self.data.extend_from_slice(&entry);
+            return true;
+        }
+
+        let data_length = self.data.len();
+        // existing data + offset data + key + val + new kv offset size + new offset size
+        if self.data.len() + data_length * 2 + key.len() + value.len() + 3 * 2 > self.block_size {
+            return false;
+        }
+        let offset = self.offsets.last().unwrap() + key.len() as u16 + value.len() as u16 + 2 * 2;
+        self.offsets.push(offset);
+
+        let (high, low) = split_u16_to_u8s(key.len() as u16);
+        let mut entry = vec![high, low];
+        entry.extend_from_slice(key.raw_ref());
+
+        let (high_v, low_v) = split_u16_to_u8s(value.len() as u16);
+        entry.push(high_v);
+        entry.push(low_v);
+        entry.extend_from_slice(value);
+        self.data.extend_from_slice(&entry);
+        true
     }
 
     /// Check if there is no key-value pair in the block.
     pub fn is_empty(&self) -> bool {
-        unimplemented!()
+        self.offsets.is_empty()
     }
 
     /// Finalize the block.
     pub fn build(self) -> Block {
-        unimplemented!()
+        Block {
+            data: self.data,
+            offsets: self.offsets,
+        }
     }
 }
